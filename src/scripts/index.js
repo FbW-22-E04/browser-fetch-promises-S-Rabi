d300917f35435378fdb175ba5efa27946248e606
S Rabi <rabi.sanaye@yahoo.com>
2022-09-28 16:02:36 +0200

    promise day  1 ex

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,17 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
-
 
 // \/ All of your javascript should go here \/
 
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    document.querySelector(".modal").style.display = "block";
+  }, 60000);
+});
+document
+  .querySelector(".close")
+  .addEventListener(
+    "click",
+    (e) => (document.querySelector(".modal").style.display = "none")
+  );
